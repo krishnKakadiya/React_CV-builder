aa97464b0e5d13b40771de4d3a93bdddf31e30c3
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-05-23 11:37:57 +0530

    solve photInput issue

--- src/components/Utils/PhotoInput.jsx
+++ src/components/Utils/PhotoInput.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-// import AddIcons  from "../../assets/add.png";
+// import AddIcon  from "../../assets/add.png";
+// import AddI from "../../../public/images/add.png"
 
 const PhotoInput = (props) => {
 
   const {onChange} = props
+  console.log(props);
   return (
+    <div style={{
+      display:"flex",
+    }}>
+
     <label>
     choose Photo
-    {/* <AddIcons /> */}
-    <input type="filr" accept='image/*' id='photo' 
+    {/* <AddIcon /> */}
+    <img style={{
+      width:'22%',
+    
+    
+
+    }} src="../../../public/images/add.png" alt="" />
+    <input type="file" accept='image/*' id='photo' 
     onChange={(e) => onChange(e)}
      />
     </label>
+    </div>
   )
 }
 
