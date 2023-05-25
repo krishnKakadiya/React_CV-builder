d62ac898e873d822887ab73ac093d3e8c80ba0c4
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-05-25 09:42:31 +0530

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className=''>
         
         <nav className="navbar navbar-expand-lg navbar-light bg-warning g fixed-top">
-  <div className="container-fluid">
+  <div className="container">
     <a className="navbar-brand" href="#">Build CV</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
