22b4b983fd0ee5e3830bc3b9c351a417cfeb7666
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-05-25 10:00:14 +0530

    style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className=''>
         
-        <nav className="navbar navbar-expand-lg navbar-light bg-warning g fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
   <div className="container">
     <a className="navbar-brand" href="#">Build CV</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent " aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
